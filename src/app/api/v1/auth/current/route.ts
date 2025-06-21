0b89f3a548254dd53c77a6518f87401e2298d728
tralwdwd <79791517+tralwdwd@users.noreply.github.com>
2025-06-21 08:05:18 +0100

    Fix errors

--- src/app/api/v1/auth/current/route.ts
+++ src/app/api/v1/auth/current/route.ts
@@ -3,7 +3,7 @@ import { getUserFromRequest } from "@/lib/auth";
 import { NextResponse } from "next/server";
 
 export async function GET(request) {
-  const userId = await getUserFromRequest(request);
+  const userId: string = await getUserFromRequest(request);
 
   if (!userId) {
     return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
