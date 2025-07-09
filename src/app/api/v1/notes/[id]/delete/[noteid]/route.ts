733def726b255b63944e1c0e4f7962e605f4bc86
tralwdwd <79791517+tralwdwd@users.noreply.github.com>
2025-07-09 23:18:44 +0100

    testing netlify

--- src/app/api/v1/notes/[id]/delete/[noteid]/route.ts
+++ src/app/api/v1/notes/[id]/delete/[noteid]/route.ts
@@ -1,3 +1,4 @@
+export const runtime = 'nodejs'; 
 import { NextRequest, NextResponse } from "next/server";
 import { initDB, Project, Note } from "@/lib/db";
 import { getUserFromRequest } from "@/lib/auth";
